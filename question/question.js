1e500e2340cbc0e4b32b73f979092aa330797c7e
Jet-ka <karkhajetlin@gmail.com>
2024-06-16 20:40:28 +0530

    all feauture of my children add

---
+++ question/question.js
@@ -0,0 +1,188 @@
+//question
+const assamhistory=[
+    {"question":"When did Muhammad-bin-Bkhtiyar Khalji Proceeded Eastward into Assam?",
+       "options":["1204","1205","1206","1207"],
+       "answer":"1206",
+       "number":"one"
+    },
+    {"question":"Who was the king of Kamrupa during Bakhtiyar Khalji's invasion in Assam?",
+    "options":["Prithu","Sandhya","Durlabh Narayan","Rup Naryan"],
+    "answer":"Prithu",
+    "number":"two"
+ },
+ {
+   "question":"Which of the following ruler was popularly known as 'Lord of Gaur'? ",
+   "options":["Durlabh Narayan","Dharma Narayan","Chandibar","Chilarai"],
+   "answer":"Dharma Narayan",
+   "number":"three"
+ },
+ {
+   "question":"Ram Saraswati,Harihar Vipra and Hem Saraswati were the court poets of which Kamata ruler?",
+   "options":["Indra Narayan","Dharma Narayan","Durlabh Narayan"],
+   "answer":"Durlabh Narayan",
+   "number":"four"
+ },
+ {
+   "question":"Who has established Cuttiya kingdom?",
+   "options":["Nitya Pal","Birpal","Nyayapal"],
+   "answer":"Birpal",
+   "number":"five"
+ },
+ {
+   "question":"In which day Sati Sadhni Divas celebrate?",
+   "options":["24 April","21 April","24 May"],
+   "answer":"21 April",
+   "number":"six"
+ },
+ {
+   "question":"What was the real name of King Nara Narayan?",
+   "options":["Narasingh","Sukhladhwaja","Malladeva"],
+   "answer":"Malladeva",
+   "number":"seven"
+ },
+ {
+   "question":"Gohain Kamal Ali from Koch-Behar to Narayanpur was constructed during the reign of",
+   "options":["Nara Narayana","Dharma Narayan","Durlabh Narayan"],
+   "answer":"Nara Narayana",
+   "number":"eight"
+ },
+ {
+   "question":"What was the real name of Chilarai?",
+   "options":["Sukhladhwaja","Biswa Singha","Malladeva"],
+   "answer":"Sukhladhwaja",
+   "number":"nine"
+ },
+ {
+   "question":"Chao Suhungmung was the first Ahom King to adopt a Hindu title,indicating a move towards an inclusive polity.Which Hindu name did he take?",
+   "options":["Swarga Narayan","Udayaditya Singha","Dhurlabh Narayana"],
+   "answer":"Swarga Narayan",
+   "number":"ten"
+ },
+ {
+   "question":"Which of the folllowing Ahom ruler was known as 'Lora Raja'(Boy King)?",
+   "options":["Sulikpha","Sujinpha","Suklampha"],
+   "answer":"Sulikpha",
+   "number":"eleven"
+ },
+ {
+   "question":"Who was sent with force to help the Manipuri King during the reign of Rajeswar Singha?",
+   "options":["Kirti Chamdra","Rudra Singha","Haranath Senapati"],
+   "answer":"Haranath Senapati",
+   "number":"twelve"
+ },
+ {
+   "question":"How many days did Captain Thomas Welsh styed in Assam during the reign of Gaurinath Singha?",
+   "options":["12Month","20Month","17Month"],
+   "answer":"17Month",
+   "number":"thirteen"
+ },
+ {
+   "question":"Who was the first Ahom ruler who stikes coins?",
+   "options":["Sulikpha","Sudangpha","Garhgayan Raja"],
+   "answer":"Garhgayan Raja",
+   "number":"fourteen"
+ },
+ {
+   "question":"Ghilajharighat Treaty was signed in",
+   "options":["23January 1663","23August 1257","21January 1662"],
+   "answer":"23January 1663",
+   "number":"fifteen"
+ },
+  {
+   "question":"Who was the first Bar Raja",
+   "options":["Sarbeswari","Deopadi","Phuleswari Devi"],
+   "answer":"Phuleswari Devi",
+   "number":"sixteen"
+ },
+  {
+   "question":"Who was known as Bamuni Konwar?",
+   "options":["Sudangpha","Dhingia Raja","Sulikpha"],
+   "answer":"Sudangpha",
+   "number":"seventeen"
+ },
+  {
+   "question":"The Battle of Saraighat between Ahoms and Mughals was fought in the year",
+   "options":["1670","1671","1663"],
+   "answer":"1671",
+   "number":"eightteen"
+ },
+ {
+   "question":"Which of the following Chief Queen of Ahom was constructed 'Shiva Doul' at Sivasagar?",
+   "options":["Deopadi","Phuleswari Devi","Sarbeswari"],
+   "answer":"Deopadi",
+   "number":"nineteen"
+ },
+  {
+   "question":"Kabiraj Chakravorty was patron by",
+   "options":["Rudra Singha","Rajeswar Singha","Pramatta Singha"],
+   "answer":"Rudra Singha",
+   "number":"twenty"
+ },
+ 
+
+
+ ]
+ 
+ const quant=[
+    {"question":"Shivam's expense is 25% more than Ramesh's expenses and Ramesh's expense is 15% less than Rupam's expenses.If the sum of their expense is Rs4660,then what would be the shivam's expenses?",
+       "options":["Rs1360","Rs1700","Rs1600","Rs1156"],
+       "answer":"Rs1700",
+       "number":"one"
+    },
+    {
+       "question":"Population of a city increses 20% every year,find the population of city after 2years if present population of a city is 15000?",
+       "options":["20600","21600","22600","19600"],
+       "answer":'21600',
+       "number":"two"
+    },
+    {
+      "question":"A and B both spend 30% of their income together which is equal to Rs 26400.If income of A is 20% more than that of B then find the income of B?",
+      "options":["52000","48000","40000"],
+      "answer":"40000",
+      "number":"three"
+    },
+    {
+      "question":"Cost price of 2 bags is in ratio 4:5 and these bags are sold at 10% profit & 20% profit respectively.Find overall profit percentage in entire transaction.",
+      "options":["15 5/9%","12 5/9%","14 5/9%"],
+      "answer":"15 5/9%",
+      "number":"four"
+    },
+    {
+      "question":"Ram sold one article at 33 1/3% profit and another at 100% profit.Find his overall profit percentage if the selling price of both the article is same?",
+      "options":["56%","60%","40%"],
+      "answer":"60%",
+      "number":"five"
+    },
+    {
+      "question":"Marked price of an article is Rs 250 more than cost price of that article and it is sold at a discount of 15% on marked price.Find the cost price of the article if profit percentage earned is 27.5%?",
+      "options":["500","600","400"],
+      "answer":"500",
+      "number":"six"
+    },
+    {
+      "question":"Rohit received Rs3456 when he invested Rs P at 12% p.a at SI for 3 years.If he invested Rs(P+4400) at 15% p.a at CI compounding annually for 2 years,then find the interest received by him?",
+      "options":["4960","4515","None"],
+      "answer":"4515",
+      "number":"seven"
+    },
+    {
+      "question":"Difference of CI received in first year and second year at 20% per annum at CI is Rs 1200 then find the sum?",
+      "options":["30000","60000","40000"],
+      "answer":"30000",
+      "number":"eight"
+    },
+    {
+      "question":"An amount of Rs 4000 is invested at 20% per annum for 2 years at CI half-yearly,then find the total interest amount received after 2 years?",
+      "options":["18550","18125","18564"],
+      "answer":"18564",
+      "number":"nine"
+    },
+    {
+      "question":"What is the interest earned by Rupam on 10000 Rupees for the period of 2 years at the rate of 12.5% SI?",
+      "options":["2000","3000","2500"],
+      "answer":"2500",
+      "number":"ten"
+    },
+ ]
+
+ export {assamhistory,quant}
--- routes/test.js
+++ routes/test.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import bodyparser from 'body-parser';
 //import mongoose from 'mongoose';
+import {assamhistory,quant} from '../question/question.js';
 const router=express.Router();
 
 router.use(bodyparser.urlencoded({extended:true}));
 
+
+
 router.get('/:id',async (req,res)=>{
     try {
        const id=req.params.id;
-     //  console.log(id)
+    //  console.log(id)
        res.render('testpaige.ejs',{infos:id}) 
     } catch (error) {
        console.log('error') 
@@ -20,24 +23,129 @@ router.post('/:id',async (req,res)=>{
       const id=req.params.id;
       const value=req.body.topic;
      // console.log(value)
+      if(value=="assamhistory"){
+         res.render('quiz.ejs',{sub:assamhistory,infos:id,value:value})
+        // console.log(assamhistory)
+      } if(value=="quant"){
+        res.render('quiz.ejs',{sub:quant,infos:id,value:value}) 
+      }
       
-      res.render('quiz.ejs',{sub:value,infos:id})
    } catch (error) {
       console.log(error)
    }
 })
-
+//result calculate after user submit their answer
 router.post('/num/:id',async (req,res)=>{
    try {
       const id=req.params.id;
-      let number=0;
-      const {subject,one}=req.body;
- if(one=='displacement'){
-   number++
+     // console.log(id);
+      let score=0;
+      //console.log(score)
+     // let wrong=0;
+         
+ const {value,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eightteen,nineteen,twenty}=req.body;
+ //for assamhistory logic     
+ if(value=='assamhistory'){
+
+ //for next question
+ 
+if(one==assamhistory[0].answer){
+   score++;
  }
- res.render('result.ejs',{id:id,subject:subject,mark:number})
+if(two==assamhistory[1].answer){
+   score++;
+}
+if(three==assamhistory[2].answer){
+   score++
+}
+
+if(four==assamhistory[3].answer){
+   score++
+}
+if(five==assamhistory[4].answer){
+  score++
+}
+if(six==assamhistory[5].answer){
+ score++
+}
+if(seven==assamhistory[6].answer){
+   score++
+}
+if(eight==assamhistory[7].answer){
+   score++
+}
+if(nine==assamhistory[8].answer){
+ score++
+}
+if(ten==assamhistory[9].answer){
+   score++
+}
+if(eleven==assamhistory[10].answer){
+   score++
+}
+if(twelve==assamhistory[11].answer){
+   score++
+}
+if(thirteen==assamhistory[12].answer){
+   score++
+}
+if(fourteen==assamhistory[13].answer){
+   score++
+}
+if(fifteen==assamhistory[14].answer){
+   score++
+}
+if(sixteen==assamhistory[15].answer){
+   score++
+}
+if(seventeen==assamhistory[16].answer){
+  score++
+  }
+if(eightteen==assamhistory[17].answer){
+   score++
+}
+if(nineteen==assamhistory[18].answer){
+   score++
+}
+if(twenty==assamhistory[19].answer){
+   score++
+}
+
+res.render('result.ejs',{mark:score,value:value,id:id})
+
+//for quant logic
+ }if(value=='quant'){
+if(one==quant[0].answer){
+   score++
+}if(two==quant[1].answer){
+   score++
+}if(three==quant[2].answer){
+   score++
+}if(four==quant[3].answer){
+   score++
+}if(five==quant[4].answer){
+   score++
+}if(six==quant[5].answer){
+   score++
+}if(seven==quant[6].answer){
+   score++
+}if(eight==quant[7].answer){
+   score++
+}if(nine==quant[8].answer){
+   score++
+}if(ten==quant[9].answer){
+   score++
+}
+
+res.render('result.ejs',{mark:score,value:value,id:id})
+ }
+
+
+
+ 
    } catch (error) {
-    console.log(error)  
+   // console.log(error)  
+   res.send("Error")
    }
 })
 
